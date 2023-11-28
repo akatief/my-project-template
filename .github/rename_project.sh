1dff750fd48e815047d03b99d7171a2880cba5b1
akatief <akatief@users.noreply.github.com>
2023-11-28 12:14:22 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="akatief"
+original_name="my_project_template"
+original_urlname="my-project-template"
+original_description="Awesome my_project_template created by akatief"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv my_project_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
